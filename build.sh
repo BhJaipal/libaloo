0a2986b9903d85307801523babe09d47ba854ecc
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-09-06 02:46:40 +0530

    test working now

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ ! -d "build/" ]; then
-	mkdir build
-fi
 
-cmake -S . -B build
-cd build
-make
+if [ $1 = "clean" ]; then
+	rm -rf build
+elif [ $1 = "test" ]; then
+	cd build && ctest -C Release && cd ..
+elif [ ! $1 ]; then
+	if [ ! -d "build/" ]; then
+		mkdir build
+	fi
+
+	cmake -B build -DCMAKE_BUILD_TYPE=Release
+	cd build
+	make
+fi
