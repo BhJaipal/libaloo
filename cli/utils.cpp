97c17ded6697e9afd5ee1cfb59e4207208eb6a62
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-09-30 18:32:55 +0530

    deciding to replace c++ with lua for TUI

--- cli/utils.cpp
+++ cli/utils.cpp
@@ -1,66 +1,31 @@
 #pragma once
+// #define ALOO_TUI
 #ifdef ALOO_TUI
+#include "ftxui/component/animation.hpp"
+#include "ftxui/component/captured_mouse.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/component_options.hpp"
+#include "ftxui/component/event.hpp"
+#include "ftxui/component/loop.hpp"
+#include "ftxui/component/mouse.hpp"
+#include "ftxui/component/receiver.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/component/task.hpp"
 #include "ftxui/dom/canvas.hpp"
 #include "ftxui/dom/direction.hpp"
 #include "ftxui/dom/elements.hpp"
+#include "ftxui/dom/flexbox_config.hpp"
+#include "ftxui/dom/linear_gradient.hpp"
+#include "ftxui/dom/node.hpp"
 #include "ftxui/dom/requirement.hpp"
 #include "ftxui/dom/table.hpp"
 #include "ftxui/screen/box.hpp"
 #include "ftxui/screen/color.hpp"
 #include "ftxui/screen/color_info.hpp"
 #include "ftxui/screen/screen.hpp"
-#endif
-#include <cstdlib>
-#include <filesystem>
-#include <iostream>
-#include <string>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <vector>
-namespace fs = std::filesystem;
-
-bool ends_with(std::string const &fullString, std::string const &ending) {
-	if (fullString.length() >= ending.length()) {
-		return (0 == fullString.compare(fullString.length() - ending.length(),
-										ending.length(), ending));
-	} else {
-		return false;
-	}
-}
-
-std::vector<std::string> split(const std::string &str,
-							   const std::string &delimiter) {
-	std::vector<std::string> tokens;
-	std::size_t pos = 0;
-	std::size_t found = str.find(delimiter);
-
-	while (found != std::string::npos) {
-		tokens.push_back(str.substr(pos, found - pos));
-		pos = found + delimiter.length();
-		found = str.find(delimiter, pos);
-	}
-
-	tokens.push_back(str.substr(pos));
-	return tokens;
-}
-
-static std::string &implode(const std::vector<std::string> &elems, char delim,
-							std::string &s) {
-	for (std::vector<std::string>::const_iterator ii = elems.begin();
-		 ii != elems.end(); ++ii) {
-		s += (*ii);
-		if (ii + 1 != elems.end()) { s += delim; }
-	}
-
-	return s;
-}
-
-static std::string implode(const std::vector<std::string> &elems, char delim) {
-	std::string s;
-	return implode(elems, delim, s);
-}
-
-#ifdef ALOO_TUI
+#include "ftxui/screen/string.hpp"
+#include "ftxui/screen/terminal.hpp"
 void help() {
 	int cols = 80;
 	int lines = 24;
@@ -114,6 +79,16 @@ void help() {
 	screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
 	Render(screen, document);
 	screen.Print();
+	std::vector<std::string> entries{"Create App", "Build", "Run", "Model",
+									 "Connect DB"};
+
+	auto screen2 = ScreenInteractive::TerminalOutput();
+	int selected = 0;
+	MenuOption option;
+	option.on_enter = screen2.ExitLoopClosure();
+	auto menu = Menu(&entries, &selected, option);
+
+	screen2.Loop(menu);
 }
 #else
 void help() {
@@ -124,3 +99,53 @@ void help() {
 	std::cout << "\n";
 }
 #endif
+
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <vector>
+namespace fs = std::filesystem;
+
+bool ends_with(std::string const &fullString, std::string const &ending) {
+	if (fullString.length() >= ending.length()) {
+		return (0 == fullString.compare(fullString.length() - ending.length(),
+										ending.length(), ending));
+	} else {
+		return false;
+	}
+}
+
+std::vector<std::string> split(const std::string &str,
+							   const std::string &delimiter) {
+	std::vector<std::string> tokens;
+	std::size_t pos = 0;
+	std::size_t found = str.find(delimiter);
+
+	while (found != std::string::npos) {
+		tokens.push_back(str.substr(pos, found - pos));
+		pos = found + delimiter.length();
+		found = str.find(delimiter, pos);
+	}
+
+	tokens.push_back(str.substr(pos));
+	return tokens;
+}
+
+static std::string &implode(const std::vector<std::string> &elems, char delim,
+							std::string &s) {
+	for (std::vector<std::string>::const_iterator ii = elems.begin();
+		 ii != elems.end(); ++ii) {
+		s += (*ii);
+		if (ii + 1 != elems.end()) { s += delim; }
+	}
+
+	return s;
+}
+
+static std::string implode(const std::vector<std::string> &elems, char delim) {
+	std::string s;
+	return implode(elems, delim, s);
+}
