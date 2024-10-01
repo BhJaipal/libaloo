b73031dfecaa86b58bd60fbaf595e7949f00594b
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-10-01 09:01:58 +0530

    working on logo

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,10 +5,18 @@ local event = ltui.event
 local rect = ltui.rect
 local window = ltui.window
 local tui = application()
+
+largeLogo = [[
+\n ██████╗   ██╗         █████╗        █████╗
+\n██╔═══██║  ██║       ██╔════██╗    ██╔════██╗
+\n████████║  ██║      ██║      ██║  ██║      ██║
+\n██║   ██║  ██║       ██╚════██╝    ██╚════██╝
+\n██║   ██╝  ██████║     █████╝        █████╝
+]]
 function tui:init()
 	application.init(self, "demo")
 	self:background_set("blue")
-	self:insert(window:new("window.main", rect({ 1, 1, self:width() - 1, self:height() - 1 }), "main window", true))
+	self:insert(window:new("window.main", rect({ 1, 1, self:width() - 1, self:height() - 1 }), largeLogo, true))
+	-- self:insert(window:new("window.main", rect({ 1, 1, self:width() - 1, self:height() - 1 }), "main window", true))
 end
-
 tui:run()
