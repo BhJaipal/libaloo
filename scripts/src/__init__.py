ec14ebf950e407ff1c8f0f25245cad1fe53c23b1
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-10-21 20:24:45 +0530

    Create App by name succeed

--- scripts/src/__init__.py
+++ scripts/src/__init__.py
@@ -1,6 +1,8 @@
 import blessed
 import os
 from typing import Union, Optional, Callable
+import yaml
+import sys
 
 term = blessed.Terminal()
 spaces: str = " "
@@ -11,7 +13,6 @@ for i in range(
 
 
 def emptyLine(end="\n"):
-    global term
     for i in range(term.width):
         print(term.royalblue_on_lightblue(" "), end="")
     print(end=end)
@@ -80,7 +81,7 @@ commandsInfo = {
 }
 
 
-def lineSpace(n: float = 1 / 4, isEnd=False):
+def lineSpace(n: float = 1 / 4, isEnd=False) -> str:
     lineSpaces = ""
     global commands
     for i in range(int(n * (term.height - 5))):
@@ -112,7 +113,7 @@ selectedCommand = ""
 commandSpace: str = ""
 
 
-def takeInput():
+def takeInput() -> None:
     try:
         global commands
         global activeSelection
@@ -281,11 +282,13 @@ class AppOptions:
     InputNumber = 0
     Name = ""
     Desc = ""
+    projectName = ""
 
 
 def __init__(self):
     self.selectedAppOption = "Name"
     self.InputNumber = 0
+    self.projectName = ""
     self.Name = ""
     self.Desc = ""
 
@@ -293,7 +296,7 @@ def __init__(self):
 appOptions = AppOptions()
 
 
-def createApp():
+def createApp() -> None:
     try:
         global commandSpace
         global appOptions
@@ -304,7 +307,7 @@ def createApp():
         global selectedCommand
         os.system("clear")
         term.fullscreen()
-        print(term.royalblue_on_lightblue(lineSpace(1 / 6)))
+        print(term.royalblue_on_lightblue(lineSpace(1 / 12)))
         print(term.cornflowerblue_on_lightblue((logo[1:-1])))
         print(term.royalblue_on_lightblue(lineSpace(1 / 6, True)))
         selectionLabel = "╓── ╓── ╥   ╓── ╓── ─╥─    ╓─╖ ╓╖ ╥ ╓──"
@@ -341,7 +344,6 @@ def createApp():
             if appOptions.selectedAppOption == "Path"
             else term.bold_cornflowerblue_on_cornflowerblue
         )
-        emptyLine()
         emptyLine()
         print(
             term.royalblue_on_lightblue(
@@ -401,6 +403,77 @@ def createApp():
             )
         )
         emptyLine()
+        # Project Name/ Path Input
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 0
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" Project Name ") - 20) // 2 + 2))
+                + (
+                    "┌─ Project {} ".format(appOptions.selectedAppOption)
+                    + ("─" * 13)
+                    + "┐"
+                )
+                + (" " * ((term.width - len(" Project Name ") - 20) // 2 + 3))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 0
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" Project Name ") - 20) // 2 + 2))
+                + (
+                    "│ "
+                    + appOptions.projectName
+                    + (" " * (27 - len(appOptions.projectName)))
+                    + "│"
+                )
+                + (" " * ((term.width - len(" Project Name ") - 20) // 2 + 3))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 0
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" Project Name ") - 20) // 2 + 2))
+                + ("├" + ("─" * 28) + "┤")
+                + (" " * ((term.width - len(" Project Name ") - 20) // 2 + 3))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 0
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" Project Name ") - 20) // 2 + 2))
+                + (
+                    "│ "
+                    + term.bold_royalblue_on_lightblue("Default: ")
+                    + "aloo-project"
+                    + (" " * (27 - len("Default: aloo-project")))
+                    + "│"
+                )
+                + (" " * ((term.width - len(" Project Name ") - 20) // 2 + 3))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 0
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" Project Name ") - 20) // 2 + 2))
+                + ("└" + ("─" * 28) + "┘")
+                + (" " * ((term.width - len(" Project Name ") - 20) // 2 + 3))
+            )
+        )
         # App Name Input
         print(
             (
@@ -420,7 +493,35 @@ def createApp():
                 else term.cornflowerblue_on_lightblue
             )(
                 (" " * ((term.width - len(" App Name ") - 20) // 2))
-                + ("│" + (" " * 28) + "│")
+                + ("│ " + appOptions.Name + (" " * (27 - len(appOptions.Name))) + "│")
+                + (" " * ((term.width - len(" App Name ") - 20) // 2 + 1))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 1
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" App Name ") - 20) // 2))
+                + ("├" + ("─" * 28) + "┤")
+                + (" " * ((term.width - len(" App Name ") - 20) // 2 + 1))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 1
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - len(" App Name ") - 20) // 2))
+                + (
+                    "│ "
+                    + term.bold_royalblue_on_lightblue("Default: ")
+                    + "Example App"
+                    + (" " * (27 - len("Default: Example App")))
+                    + "│"
+                )
                 + (" " * ((term.width - len(" App Name ") - 20) // 2 + 1))
             )
         )
@@ -442,9 +543,9 @@ def createApp():
                 if appOptions.InputNumber == 2
                 else term.cornflowerblue_on_lightblue
             )(
-                (" " * ((term.width - len(" Description ") - 20) // 2))
-                + ("┌─ Description " + ("─" * 17) + "┐")
-                + (" " * ((term.width - len(" Description ") - 20) // 2))
+                (" " * ((term.width - 13 - 40) // 2))
+                + ("┌─ Description " + ("─" * 37) + "┐")
+                + (" " * ((term.width - 13 - 40) // 2))
             )
         )
         print(
@@ -453,9 +554,9 @@ def createApp():
                 if appOptions.InputNumber == 2
                 else term.cornflowerblue_on_lightblue
             )(
-                (" " * ((term.width - len(" Description ") - 20) // 2))
-                + ("│" + (" " * (18 + len(" Description "))) + "│")
-                + (" " * ((term.width - len(" Description ") - 20) // 2))
+                (" " * ((term.width - 13 - 40) // 2))
+                + ("│ " + appOptions.Desc + (" " * (50 - len(appOptions.Desc))) + "│")
+                + (" " * ((term.width - 13 - 40) // 2))
             )
         )
         print(
@@ -464,13 +565,84 @@ def createApp():
                 if appOptions.InputNumber == 2
                 else term.cornflowerblue_on_lightblue
             )(
-                (" " * ((term.width - len(" Description ") - 20) // 2))
-                + ("└" + ("─" * (18 + len(" Description "))) + "┘")
-                + (" " * ((term.width - len(" Description ") - 20) // 2))
+                (" " * ((term.width - 53) // 2))
+                + ("├" + ("─" * 51) + "┤")
+                + (" " * ((term.width - 53) // 2))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 2
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - 53) // 2))
+                + (
+                    "│ "
+                    + term.bold_royalblue_on_lightblue("Default: ")
+                    + "An default template app for aloo project"
+                    + (
+                        " "
+                        * (
+                            50
+                            - len("Default: An default template app for aloo project")
+                        )
+                    )
+                    + "│"
+                )
+                + (" " * ((term.width - 53) // 2))
+            )
+        )
+        print(
+            (
+                term.royalblue_on_lightblue
+                if appOptions.InputNumber == 2
+                else term.cornflowerblue_on_lightblue
+            )(
+                (" " * ((term.width - 53) // 2))
+                + ("└" + ("─" * (18 + 33)) + "┘")
+                + (" " * ((term.width - 53) // 2))
             )
         )
         emptyLine()
-        print(term.royalblue_on_lightblue(lineSpace(1 / 4)))
+        print(
+            term.royalblue_on_lightblue(" " * ((term.width - 9) // 2))
+            + (
+                term.royalblue_on_royalblue
+                if appOptions.InputNumber == 3
+                else term.cornflowerblue_on_cornflowerblue
+            )("█" * 9)
+            + term.royalblue_on_lightblue(" " * ((term.width - 9) // 2))
+        )
+        print(
+            term.royalblue_on_lightblue(" " * ((term.width - 9) // 2))
+            + (
+                term.royalblue_on_royalblue
+                if appOptions.InputNumber == 3
+                else term.cornflowerblue_on_cornflowerblue
+            )("██")
+            + (
+                term.bold_lightblue_on_royalblue
+                if appOptions.InputNumber == 3
+                else term.bold_lightblue_on_cornflowerblue
+            )("Enter")
+            + (
+                term.royalblue_on_royalblue
+                if appOptions.InputNumber == 3
+                else term.cornflowerblue_on_cornflowerblue
+            )("██")
+            + term.royalblue_on_lightblue(" " * ((term.width - 9) // 2))
+        )
+        print(
+            term.royalblue_on_lightblue(" " * ((term.width - 9) // 2))
+            + (
+                term.royalblue_on_royalblue
+                if appOptions.InputNumber == 3
+                else term.cornflowerblue_on_cornflowerblue
+            )("█" * 9)
+            + term.royalblue_on_lightblue(" " * ((term.width - 9) // 2))
+        )
+        print(term.royalblue_on_lightblue(lineSpace(1 / 12)))
         if isCommandEnabled:
             commandSpace = ""
             for _ in range(int((term.width - exitCommand.__len__() - 11))):
@@ -485,62 +657,135 @@ def createApp():
             emptyLine("")
         with term.cbreak(), term.hidden_cursor():
             inp = term.inkey()
-            print(inp if inp.name is None else inp.name)
-            if inp.name is not None and inp.name == "KEY_ESCAPE":
-                isCommandEnabled = True
-            if inp.name is not None and inp.name == "KEY_LEFT":
-                appOptions.selectedAppOption = "Name"
-            if inp.name is not None and inp.name == "KEY_RIGHT":
-                appOptions.selectedAppOption = "Path"
-            if inp.name is not None and inp.name == "KEY_DOWN":
-                if appOptions.InputNumber == 2:
-                    appOptions.InputNumber = 0
+            if isCommandEnabled:
+                if inp.name is None:
+                    exitCommand += inp
+                elif inp.name is not None:
+                    if inp.name == "KEY_ENTER" and exitCommand == ":q":
+                        os.system("clear")
+                        exit(0)
+                    elif inp.name == "KEY_BACKSPACE":
+                        if exitCommand.__len__() > 0:
+                            exitCommand = exitCommand[:-1]
+                        else:
+                            exitCommand = ""
+                            isCommandEnabled = False
+            else:
+                if inp.name is not None:
+                    if inp.name == "KEY_ESCAPE":
+                        isCommandEnabled = True
+                    elif inp.name == "KEY_LEFT":
+                        appOptions.selectedAppOption = "Name"
+                    elif inp.name == "KEY_RIGHT":
+                        appOptions.selectedAppOption = "Path"
+                    elif inp.name == "KEY_DOWN":
+                        if appOptions.InputNumber == 3:
+                            appOptions.InputNumber = 0
+                        else:
+                            appOptions.InputNumber += 1
+                    elif inp.name == "KEY_UP":
+                        if appOptions.InputNumber == 0:
+                            appOptions.InputNumber = 3
+                        else:
+                            appOptions.InputNumber -= 1
+                    elif inp.name == "KEY_BACKSPACE":
+                        match appOptions.InputNumber:
+                            case 1:
+                                if appOptions.Name.__len__() > 0:
+                                    appOptions.Name = appOptions.Name[:-1]
+                                else:
+                                    appOptions.Name = ""
+                            case 0:
+                                if appOptions.projectName.__len__() > 0:
+                                    appOptions.projectName = appOptions.projectName[:-1]
+                                else:
+                                    appOptions.Desc = ""
+                            case 2:
+                                if appOptions.projectName.__len__() > 0:
+                                    appOptions.projectName = appOptions.projectName[:-1]
+                                else:
+                                    appOptions.projectName = ""
+                    elif inp.name == "KEY_ENTER" and appOptions.InputNumber == 3:
+                        appInfo = {
+                            "app-name": (
+                                appOptions.Name
+                                if len(appOptions.Name) != 0
+                                else "Example App"
+                            ),
+                            "project-name": (
+                                appOptions.projectName
+                                if len(appOptions.projectName) != 0
+                                else "aloo-project"
+                            ),
+                            "description": (
+                                appOptions.Desc
+                                if len(appOptions.Desc) != 0
+                                else "An default template app for aloo project"
+                            ),
+                        }
+                        if "aloo.config.yaml" in os.listdir(os.getcwd()):
+                            os.system("clear")
+                            raise FileExistsError("'aloo.config.yaml' already exist")
+                        currFilePath = __file__.removesuffix("__init__.py")
+                        os.mkdir(appInfo["project-name"])
+                        yaml.dump(
+                            appInfo,
+                            open(appInfo["project-name"] + "/aloo.config.yaml", "w+"),
+                        )
+                        os.mkdir(appInfo["project-name"] + "/src")
+                        os.mkdir(appInfo["project-name"] + "/test")
+                        os.system("clear")
+
+                        mainC = open(appInfo["project-name"] + "/src/main.c", "w+")
+                        testC = open(appInfo["project-name"] + "/test/main.c", "w+")
+                        CMakeFile = open(
+                            appInfo["project-name"] + "/CMakeLists.txt", "w+"
+                        )
+
+                        mainCSample = open(
+                            currFilePath + "main.c",
+                            "r",
+                        ).read()
+
+                        testCSample = open(
+                            currFilePath + "test.c",
+                            "r",
+                        ).read()
+
+                        CMakeSample = open(
+                            currFilePath + "CMakeLists.txt",
+                            "r",
+                        ).read()
+                        alooDir = currFilePath.removesuffix("scripts/src/")
+
+                        mainCSample = mainCSample.replace(
+                            "$projectName", appInfo["project-name"]
+                        ).replace("$appName", appInfo["app-name"])
+                        mainC.write(mainCSample)
+
+                        testCSample = testCSample.replace(
+                            "$projectName", appInfo["project-name"]
+                        ).replace("$appName", appInfo["app-name"])
+                        testC.write(testCSample)
+
+                        CMakeSample = CMakeSample.replace(
+                            "$appName", appInfo["app-name"]
+                        ).replace("$include_dir", alooDir + "include")
+                        CMakeSample = CMakeSample.replace(
+                            "$libaloo", alooDir + "lib"
+                        ).replace("$libaloo", alooDir + "lib")
+                        CMakeFile.write(CMakeSample)
+
+                        print("\t\033[1;32m󰄭 Aloo project created successfully\033[0m")
+                        exit(0)
                 else:
-                    appOptions.InputNumber += 1
-            if inp.name is not None and inp.name == "KEY_UP":
-                if appOptions.InputNumber == 0:
-                    appOptions.InputNumber = 2
-                else:
-                    appOptions.InputNumber -= 1
-            elif inp.name is None and not isCommandEnabled:
-                if appOptions.InputNumber == 1:
-                    appOptions.Name += inp
-                elif appOptions.InputNumber == 2:
-                    appOptions.Desc += inp
-            elif inp.name is None and isCommandEnabled:
-                exitCommand += inp
-            elif (
-                inp.name is not None
-                and not isCommandEnabled
-                and inp.name == "KEY_BACKSPACE"
-            ):
-                if appOptions.InputNumber == 1:
-                    if appOptions.Name.__len__() > 0:
-                        appOptions.Name = appOptions.Name[:-1]
-                    else:
-                        appOptions.Name = ""
-                elif appOptions.InputNumber == 2:
-                    if appOptions.Desc.__len__() > 0:
-                        appOptions.Desc = appOptions.Desc[:-1]
-                    else:
-                        appOptions.Desc = ""
-            elif (
-                inp.name is not None
-                and isCommandEnabled
-                and inp.name == "KEY_BACKSPACE"
-            ):
-                if exitCommand.__len__() > 0:
-                    exitCommand = exitCommand[:-1]
-                else:
-                    isCommandEnabled = False
-            if (
-                inp.name is not None
-                and inp.name == "KEY_ENTER"
-                and isCommandEnabled
-                and exitCommand == ":q"
-            ):
-                os.system("clear")
-                exit(0)
+                    match appOptions.InputNumber:
+                        case 0:
+                            appOptions.projectName += inp
+                        case 1:
+                            appOptions.Name += inp
+                        case 2:
+                            appOptions.Desc += inp
             createApp()
     except KeyboardInterrupt:
         print("Process exited")
