26a6a516469f26a77dd1be11dcfc539bb41f34a7
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-10-23 19:05:34 +0530

    correction in moving and installs

--- release.sh
+++ release.sh
@@ -9,19 +9,15 @@ make
 cd ..
 mv scripts/src/__init__.py bin/aloo.py
 
-if [ ! $(which tree) ]; then
-	sudo apt install tree
-fi
-
-mv -r dist/deb/etc etc
-mkdir etc/aloo
+mkdir -p etc/aloo
+echo "PKG_CONFIG_PATH=/usr/share/pkgconfig/aloo.pc\n" >etc/aloo/aloo.conf
 mv scripts/src/main.c etc/aloo/main.c
 mv scripts/src/test.c etc/aloo/test.c
 mv scripts/src/main.server.c etc/aloo/main.server.c
 mv scripts/src/main.server.h etc/aloo/main.server.h
 mv scripts/src/CMakeLists.txt etc/aloo/CMakeLists.txt
-mkdir DEBIAN
 
+mkdir DEBIAN
 echo "Package: aloo
 Version: $1
 Section: utils
@@ -32,10 +28,12 @@ Description: This a library based on Gtk4 written in C to make things easier
 Depends: libgtk-4-dev, libsqlite3-dev, python3, python3-pip, make
 " >DEBIAN/control
 
-mkdir -p usr/include
-mv -r include usr/include/aloo
-mv -r dist/deb/usr/share usr/share
-rm usr/share/pkgconfig/aloo.pc
+mkdir -p usr/share/pkgconfig
+mkdir include usr/include
+mv include usr/include/aloo
+mv lib usr/lib
+mv bin usr/bin
+rm -r bin/test-1.exe
 
 echo "Name: aloo
 Version: $1
@@ -44,8 +42,6 @@ Depends: libgtk-4-dev, libsqlite3-dev, python3, python3-pip, make
 CFlags: -I/usr/include/aloo
 Libs: -laloo" >usr/share/pkgconfig/aloo.pc
 
-mv -r lib usr/lib
-mv -r bin usr/bin
 rm -r *.github
 rm -r *.vscode
 rm -r include
@@ -56,7 +52,8 @@ rm -r scripts
 rm -r assets
 rm -r build
 rm -r aloo-edit
-rm -r dist,docs
+rm -r dist
+rm -r docs
 rm -r *.png
 rm -r CMakeLists.txt
 rm -r build.sh
@@ -67,7 +64,4 @@ rm -r *.log
 rm -r *.sh
 rm -r src
 rm -r *.html
-rm -r etc/aloo
-rm -r bin/test-1.exe
 sudo dpkg-deb --root-owner-group --build . libaloo-$1.deb
-tree
